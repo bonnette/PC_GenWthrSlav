1040c7c2ef12f7d413723571546e4a9932dfac0a
Larry Bonnette <bonnette_l@hotmail.com>
2021-11-27 09:44:32 -0600

    Add project files.

---
+++ GenWthrSlav/Form1.Designer.vb
@@ -0,0 +1,134 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.lbl_dwnloaded = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.lbl_temp = New System.Windows.Forms.Label()
+        Me.lbl_humid = New System.Windows.Forms.Label()
+        Me.lbl_feels = New System.Windows.Forms.Label()
+        Me.lbl_speed = New System.Windows.Forms.Label()
+        Me.lbl_deg = New System.Windows.Forms.Label()
+        Me.lbl_gust = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lbl_dwnloaded
+        '
+        Me.lbl_dwnloaded.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lbl_dwnloaded.Location = New System.Drawing.Point(12, 9)
+        Me.lbl_dwnloaded.Name = "lbl_dwnloaded"
+        Me.lbl_dwnloaded.Size = New System.Drawing.Size(729, 115)
+        Me.lbl_dwnloaded.TabIndex = 0
+        Me.lbl_dwnloaded.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 277)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(91, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Get Weather"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'lbl_temp
+        '
+        Me.lbl_temp.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lbl_temp.Location = New System.Drawing.Point(13, 144)
+        Me.lbl_temp.Name = "lbl_temp"
+        Me.lbl_temp.Size = New System.Drawing.Size(93, 35)
+        Me.lbl_temp.TabIndex = 2
+        Me.lbl_temp.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lbl_humid
+        '
+        Me.lbl_humid.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lbl_humid.Location = New System.Drawing.Point(211, 144)
+        Me.lbl_humid.Name = "lbl_humid"
+        Me.lbl_humid.Size = New System.Drawing.Size(93, 35)
+        Me.lbl_humid.TabIndex = 4
+        Me.lbl_humid.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lbl_feels
+        '
+        Me.lbl_feels.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lbl_feels.Location = New System.Drawing.Point(112, 144)
+        Me.lbl_feels.Name = "lbl_feels"
+        Me.lbl_feels.Size = New System.Drawing.Size(93, 35)
+        Me.lbl_feels.TabIndex = 5
+        Me.lbl_feels.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lbl_speed
+        '
+        Me.lbl_speed.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lbl_speed.Location = New System.Drawing.Point(409, 144)
+        Me.lbl_speed.Name = "lbl_speed"
+        Me.lbl_speed.Size = New System.Drawing.Size(93, 35)
+        Me.lbl_speed.TabIndex = 6
+        Me.lbl_speed.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lbl_deg
+        '
+        Me.lbl_deg.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lbl_deg.Location = New System.Drawing.Point(310, 144)
+        Me.lbl_deg.Name = "lbl_deg"
+        Me.lbl_deg.Size = New System.Drawing.Size(93, 35)
+        Me.lbl_deg.TabIndex = 7
+        Me.lbl_deg.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lbl_gust
+        '
+        Me.lbl_gust.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lbl_gust.Location = New System.Drawing.Point(508, 144)
+        Me.lbl_gust.Name = "lbl_gust"
+        Me.lbl_gust.Size = New System.Drawing.Size(93, 35)
+        Me.lbl_gust.TabIndex = 8
+        Me.lbl_gust.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(757, 354)
+        Me.Controls.Add(Me.lbl_gust)
+        Me.Controls.Add(Me.lbl_deg)
+        Me.Controls.Add(Me.lbl_speed)
+        Me.Controls.Add(Me.lbl_feels)
+        Me.Controls.Add(Me.lbl_humid)
+        Me.Controls.Add(Me.lbl_temp)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.lbl_dwnloaded)
+        Me.Name = "Form1"
+        Me.Text = "Weather "
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents lbl_dwnloaded As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents lbl_temp As Label
+    Friend WithEvents lbl_humid As Label
+    Friend WithEvents lbl_feels As Label
+    Friend WithEvents lbl_speed As Label
+    Friend WithEvents lbl_deg As Label
+    Friend WithEvents lbl_gust As Label
+End Class
---
+++ GenWthrSlav/Form1.vb
@@ -0,0 +1,137 @@
+﻿Imports WinSCP
+Imports Newtonsoft.Json
+Imports Newtonsoft.Json.Linq
+
+' This Windows application uses "SCP" to copy the wthr.dat file from a Raspberry Pi weather client to the windows system running this app.
+' The wthr.dat file contains local weather information that was gathered from a local weather website. This data is formatted as JSON and looks like this:
+'
+'{"lat":30.22,"lon":-95.36,"timezone":"America/Chicago","timezone_offset":-21600,"current":{"dt":1637771625,"sunrise":1637758495,"sunset":1637796116,
+'"temp":73.26,"feels_like":73.58,"pressure":1019,"humidity":70,"dew_point":62.89,"uvi":1.08,"clouds":98,"visibility":10000,"wind_speed":10,"wind_deg":65,
+'"wind_gust":13,"weather":[{"id":804,"main":"Clouds","description":"overcast clouds","icon":"04d"}]}}
+'
+' The reason we copy data from a local Raspberry pi is to save money. The source website only allows 60 copies of its data for free.
+'
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' Clear the JSON text field and then prepare SCP to copy data.
+        lbl_dwnloaded.Text = ""
+        Try
+            ' Setup session options
+            ' Set up session options
+            Dim sessionOptions As New SessionOptions
+            With sessionOptions
+                .Protocol = Protocol.Sftp
+                .HostName = "Pi Hostname"
+                .UserName = "Pi Username"
+                .Password = "Pi Password"
+                .SshHostKeyFingerprint = "ssh-ed25519 255 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx="
+            End With
+            ' Connect to the Raspberry Pi
+            Using session As New Session
+                ' Connect
+                session.Open(sessionOptions)
+
+                ' Download files
+                Dim transferOptions As New TransferOptions
+                transferOptions.TransferMode = TransferMode.Binary
+
+                Dim transferResult As TransferOperationResult
+                transferResult =
+                    session.GetFiles("/home/pi/wthr.dat", "C:\Users\bonne\Desktop\", False, transferOptions)
+
+                ' Throw on any error
+                transferResult.Check()
+
+                ' Print results
+                For Each transfer In transferResult.Transfers
+                    Console.WriteLine("Download of {0} succeeded", transfer.FileName)
+                Next
+            End Using
+
+
+        Catch a As Exception
+            Console.WriteLine("Error: {0}", a)
+        End Try
+        ' We have just copied the JSON filled weather  file from the Pi we will now read the contents of the file into "fileReader" Then display the contents of
+        ' "fileReader" in the large label in the App.
+
+        Dim fileReader As String
+        fileReader = My.Computer.FileSystem.ReadAllText("C:\Users\bonne\Desktop\wthr.dat")
+        lbl_dwnloaded.Text = fileReader
+        'MsgBox(fileReader)
+
+        ' Now we parse the contents of fileReader in order to get all of the weather information out of it.
+
+        ' Set up variables for wind direction conversion
+        Dim deg As Double
+        Dim ndeg As String
+        Dim jsonObject As JObject = JObject.Parse(fileReader)
+        ' Display the temperature, humidity, Wind speed and direction ETC.
+        lbl_temp.Text = "Temp: " & jsonObject.SelectToken("current.temp").ToString
+        lbl_humid.Text = "Humidity: " & jsonObject.SelectToken("current.humidity").ToString
+        lbl_feels.Text = "Feels Like: " & jsonObject.SelectToken("current.feels_like").ToString
+        'lbl_deg.Text = "Dir: " & jsonObject.SelectToken("current.wind_deg").ToString
+        deg = jsonObject.SelectToken("current.wind_deg")
+        ndeg = DirCalc(deg)
+        lbl_deg.Text = "Dir: " & ndeg
+        lbl_speed.Text = "Speed: " & jsonObject.SelectToken("current.wind_speed").ToString
+        ' Sometimes the weather service does not send "current.wind_gust" information. So, we must
+        ' test to see if it exsists first then deal with the resuluts.
+        If jsonObject.SelectToken("current.wind_gust") Is Nothing Then
+            lbl_gust.Text = "Gust: 0"
+        Else
+            lbl_gust.Text = "Gust: " & jsonObject.SelectToken("current.wind_gust").ToString
+        End If
+        'lbl_gust.Text = "Gust: " & jsonObject.SelectToken("current.wind_gust").ToString
+        'MsgBox("Temp: " & jsonObject.SelectToken("current.temp").ToString)
+
+    End Sub
+
+    ' Wind direction conversion function returns string with wind direction in it
+    Public Shared Function DirCalc(x)
+
+        Select Case (x)
+            Case 0 To 22
+                'MsgBox(x)
+                Return "North"
+            Case 22 To 45
+                Return "NNE"
+            Case 45 To 67
+                Return "NE"
+            Case 67 To 90
+                Return "ENE"
+            Case 90 To 112
+                Return "East"
+            Case 112 To 135
+                Return "ESE"
+            Case 135 To 157
+                Return "SE"
+            Case 157 To 180
+                Return "SSE"
+            Case 180 To 202
+                Return "South"
+            Case 202 To 225
+                Return "SW"
+            Case 225 To 247
+                Return "SW"
+            Case 247 To 270
+                Return "WSW"
+            Case 270 To 292
+                Return "West"
+            Case 292 To 315
+                Return "WNW"
+            Case 315 To 337
+                Return "NW"
+            Case 337 To 359
+                Return "NNW"
+            Case Else
+                Return "No Dir"
+        End Select
+
+    End Function
+End Class
+
